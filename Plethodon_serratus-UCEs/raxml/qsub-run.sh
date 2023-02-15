4c7a0e598dc5ea6132683f4d906d0654b3cff3d9
Cathy Newman <newma014@gmail.com>
2023-02-15 11:11:59 -0600

    Add files via upload

---
+++ Plethodon_serratus-UCEs/raxml/qsub-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+qsub raxml-60-1k-rboot-1000.qsub
+qsub raxml-60-1k-rboot-100.qsub
+qsub raxml-80-1k-rboot-1000.qsub
+qsub raxml-80-1k-rboot-100.qsub
+qsub raxml-all-60-rboot-1000.qsub
+qsub raxml-all-60-rboot-100.qsub
+qsub raxml-all-80-rboot-1000.qsub
+qsub raxml-all-80-rboot-100.qsub
