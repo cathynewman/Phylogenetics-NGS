c5c3648f379db4599f65ef97c1a0722801089746
Cathy Newman <newma014@gmail.com>
2023-02-15 11:08:01 -0600

    Add files via upload

---
+++ Plethodon_serratus-UCEs/random shell scripts/contigs_copy.sh
@@ -0,0 +1,28 @@
+cp trinity1/assemblies_trinity/CEN172_trinity/Trinity.fasta contigs/CEN172.fasta
+cp trinity1/assemblies_trinity/CEN173_trinity/Trinity.fasta contigs/CEN173.fasta
+cp trinity1/assemblies_trinity/CEN174_trinity/Trinity.fasta contigs/CEN174.fasta
+cp trinity1/assemblies_trinity/CEN175_trinity/Trinity.fasta contigs/CEN175.fasta
+cp trinity9/assemblies_trinity/OMNH3_trinity/Trinity.fasta contigs/OMNH3.fasta
+cp trinity9/assemblies_trinity/OMNH4_trinity/Trinity.fasta contigs/OMNH4.fasta
+cp trinity9/assemblies_trinity/UAHC15971_trinity/Trinity.fasta contigs/UAHC15971.fasta
+cp trinity9/assemblies_trinity/UAHC15975_trinity/Trinity.fasta contigs/UAHC15975.fasta
+cp trinity9/assemblies_trinity/UAHC15978_trinity/Trinity.fasta contigs/UAHC15978.fasta
+cp trinity9/assemblies_trinity/UAHC16006_trinity/Trinity.fasta contigs/UAHC16006.fasta
+
+cp trinity2/assemblies_trinity/LSUMZ15537_trinity/Trinity.fasta contigs/LSUMZ15537.fasta
+cp trinity2/assemblies_trinity/LSUMZ18176_trinity/Trinity.fasta contigs/LSUMZ18176.fasta
+cp trinity2/assemblies_trinity/LSUMZ18530_trinity/Trinity.fasta contigs/LSUMZ18530.fasta
+cp trinity2/assemblies_trinity/LSUMZ18531_trinity/Trinity.fasta contigs/LSUMZ18531.fasta
+
+cp trinity7/assemblies_trinity/LSUMZ18817_trinity/Trinity.fasta contigs/LSUMZ18817.fasta
+cp trinity7/assemblies_trinity/LSUMZ18818_trinity/Trinity.fasta contigs/LSUMZ18818.fasta
+cp trinity7/assemblies_trinity/LSUMZ18820_trinity/Trinity.fasta contigs/LSUMZ18820.fasta
+cp trinity7/assemblies_trinity/LSUMZ18878_trinity/Trinity.fasta contigs/LSUMZ18878.fasta
+cp trinity7/assemblies_trinity/LSUMZ19190_trinity/Trinity.fasta contigs/LSUMZ19190.fasta
+
+cp trinity8/assemblies_trinity/LSUMZ1931_trinity/Trinity.fasta contigs/LSUMZ1931.fasta
+cp trinity8/assemblies_trinity/LSUMZ2074_trinity/Trinity.fasta contigs/LSUMZ2074.fasta
+cp trinity8/assemblies_trinity/LSUMZ21030_trinity/Trinity.fasta contigs/LSUMZ21030.fasta
+cp trinity8/assemblies_trinity/LSUMZ2199_trinity/Trinity.fasta contigs/LSUMZ2199.fasta
+cp trinity8/assemblies_trinity/LSUMZ2534_trinity/Trinity.fasta contigs/LSUMZ2534.fasta
+cp trinity8/assemblies_trinity/LSUMZ854_trinity/Trinity.fasta contigs/LSUMZ854.fasta
